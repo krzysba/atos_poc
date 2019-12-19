5164be9296969d13d5e26aaa2b0b3ec9429aaf76
student <student@node4.trainingdocker>
2019-12-20 00:02:26 +0100

    preparation1

--- vars.tf
+++ vars.tf
@@ -1,8 +1,10 @@
-variable "AWS_ACCESS_KEY" {
-}
+#commented resources needed from local machine
 
-variable "AWS_SECRET_KEY" {
-}
+#variable "AWS_ACCESS_KEY" {
+#}
+#
+#variable "AWS_SECRET_KEY" {
+#}
 
 variable "AWS_REGION" {
   default = "eu-central-1"
